5d4d4e30a4295b6e5ecc5f846f3e92498eb27910
章博 <brandonfaustine@gmail.com>
2023-06-03 11:05:48 +0800

    调整示例代码

--- src/datakit-sdk-cpp/ft-sdk-wrapper-sample/Program.cs
+++ src/datakit-sdk-cpp/ft-sdk-wrapper-sample/Program.cs
@@ -5,16 +5,17 @@ namespace ft_sdk_wrapper_sample
 {
     class Program
     {
-
+        const string FAKE_URL= "https://www.guance.com/path_test/?param=1";
+        //用于指定加载 dll 路径
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool SetDllDirectory(string lpPathName);
         static void Main(string[] args)
         {
 
+            //指定 dll 生成路径或将执行 dll 文件，将 dll 拷贝至 c# 执行 bin\debug\ 对应目录下
             string dllPath = "C:\\Users\\dell\\Desktop\\ft-sdk-cpp-develop\\ft-sdk-cpp-develop\\src\\datakit-sdk-cpp\\x64\\Debug";
             SetDllDirectory(dllPath);
-
 
             FTWrapper.Install("{\"serverUrl\":\"http://10.100.64.166:9529\",\"envType\":\"prod\",\"serviceName\":\"Your Services\",\"globalContext\":{\"custom_key\":\"custom value\"}}");
 
@@ -36,18 +37,18 @@ namespace ft_sdk_wrapper_sample
 
             FTWrapper.AddError("error", "error msg","","native_crash");
 
-            string resourceId = "1111";
+            string resourceId = "unique_uuid_xxxx";
 
-            IntPtr headData = FTWrapper.GetTraceHeader(resourceId,"https://www.guance.com");
+            FTWrapper.StartResource(resourceId);
+
+            FTWrapper.StopResource(resourceId);
+
+            IntPtr headData = FTWrapper.GetTraceHeader(resourceId,FAKE_URL);
             string helloJsonString = Marshal.PtrToStringAnsi(headData);
 
             Console.WriteLine(helloJsonString);
 
-          /*  FTWrapper.StartResource(resourceId);
-
-            FTWrapper.StopResource(resourceId);
-
-            FTWrapper.AddResource(resourceId, "{\"resourceStatus\":200}", "{}");*/
+            FTWrapper.AddResource(resourceId, "{\"resourceStatus\":200,\"url\":\""+ FAKE_URL + "\"}", "{}");
 
             FTWrapper.StopView();
   
